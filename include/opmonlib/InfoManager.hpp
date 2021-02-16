7fbd976480f7daf196bffd70fe908a1853ce6b34
glehmannmiotto <giovanna.lehmann@cern.ch>
2021-02-16 21:40:07 +0100

    removed old files; added running thread to InfoManager

--- include/opmonlib/InfoManager.hpp
+++ include/opmonlib/InfoManager.hpp
@@ -1,9 +1,10 @@
 #ifndef OPMONLIB_INCLUDE_OPMONLIB_INFOMANAGER_HPP_
 #define OPMONLIB_INCLUDE_OPMONLIB_INFOMANAGER_HPP_
 
-#include <iostream>
 #include <nlohmann/json.hpp>
 #include "opmonlib/InfoProvider.hpp"
+#include <atomic>
+#include <thread>
 
 namespace dunedaq::opmonlib {
 
@@ -13,13 +14,17 @@ namespace dunedaq::opmonlib {
       InfoManager( std::string service ); // Constructor, std::string is placeholder for instancing each info service
       void publish_info( int level );
       nlohmann::json gather_info( int level);
-      void set_provider( opmonlib::InfoProvider &p );
+      void set_provider( opmonlib::InfoProvider& p );
     
     private:
-      uint32_t m_time_interval_sec;
-      //const DAQModuleManager& m_mod_mgr; No longer needed?
-      opmonlib::InfoProvider* m_ip;
+      void start(uint32_t interval_sec, uint32_t level);
+      void run(uint32_t interval_sec, uint32_t level);
+      void stop();
+ 
+      mutable opmonlib::InfoProvider *m_ip = nullptr;
       std::string m_service;
+      std::atomic<bool> m_running; 
+      std::thread m_thread;
 
   };
 
--- src/InfoManager.cpp
+++ src/InfoManager.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 InfoManager::InfoManager( std::string service ) {
   m_service = service;
+  m_running.store(false);
 }
 
 
@@ -29,6 +30,7 @@ nlohmann::json InfoManager::gather_info( int level ) {
 
   nlohmann::json j;
   dunedaq::opmonlib::InfoCollector ic;
+  // FIXME: check against nullptr!
   m_ip->gather_stats(ic, level);       
   j = ic.get_collected_infos();
   
@@ -37,11 +39,34 @@ nlohmann::json InfoManager::gather_info( int level ) {
 }
 
 
-void InfoManager::set_provider( opmonlib::InfoProvider &p ){
+void InfoManager::set_provider( opmonlib::InfoProvider& p ){
 
   // Set the data member to point to selected InfoProvider
   m_ip = &p;
 
 }
 
+void InfoManager::start(uint32_t interval_sec, uint32_t level) {
+  m_running.store(true);
+  m_thread = std::thread(&InfoManager::run, this, interval_sec, level);
+}
+
+void InfoManager::run(uint32_t interval_sec, uint32_t level) { 
+ uint32_t countdown = 10 * interval_sec;
+ while(m_running.load()) {
+    if (countdown > 0) {
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      --countdown;
+    }
+    else {
+      publish_info(level);
+      countdown = 10 * interval_sec;
+    }     
+ }
+}
+
+void InfoManager::stop() {
+  m_running.store(false);
+  m_thread.join();
+}
 
