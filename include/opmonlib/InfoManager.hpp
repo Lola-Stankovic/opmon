86e86a02e71745957a9ab3fca3d9858bdbc139dc
Eric Flumerfelt <eflumerf@fnal.gov>
2021-05-26 13:16:10 -0500

    Fix linting issues and run clang-format

--- include/opmonlib/InfoManager.hpp
+++ include/opmonlib/InfoManager.hpp
@@ -1,35 +1,46 @@
+/**
+ * @file InfoManager.hpp
+ *
+ * This is part of the DUNE DAQ Application Framework, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+
 #ifndef OPMONLIB_INCLUDE_OPMONLIB_INFOMANAGER_HPP_
 #define OPMONLIB_INCLUDE_OPMONLIB_INFOMANAGER_HPP_
 
-#include <nlohmann/json.hpp>
 #include "opmonlib/InfoProvider.hpp"
 #include "opmonlib/OpmonService.hpp"
+
+#include <nlohmann/json.hpp>
+
 #include <atomic>
+#include <memory>
+#include <string>
 #include <thread>
 
 namespace dunedaq::opmonlib {
 
-  class InfoManager 
-  {
-    public:
-      InfoManager( std::string service ); // Constructor
-      InfoManager( dunedaq::opmonlib::OpmonService& service );
-      void publish_info( int level );
-      nlohmann::json gather_info( int level);
-      void set_provider( opmonlib::InfoProvider& p );
-      void start(uint32_t interval_sec, uint32_t level);
-      void stop();
-    
-    private:
-      void run(uint32_t interval_sec, uint32_t level);
- 
-      mutable opmonlib::InfoProvider *m_ip = nullptr;
-      std::shared_ptr<opmonlib::OpmonService> m_service;
-      std::atomic<bool> m_running; 
-      std::thread m_thread;
+class InfoManager
+{
+public:
+  explicit InfoManager(std::string service); // Constructor
+  explicit InfoManager(dunedaq::opmonlib::OpmonService& service);
+  void publish_info(int level);
+  nlohmann::json gather_info(int level);
+  void set_provider(opmonlib::InfoProvider& p);
+  void start(uint32_t interval_sec, uint32_t level); // NOLINT(build/unsigned)
+  void stop();
 
-  };
+private:
+  void run(uint32_t interval_sec, uint32_t level); // NOLINT(build/unsigned)
 
-}
+  mutable opmonlib::InfoProvider* m_ip = nullptr;
+  std::shared_ptr<opmonlib::OpmonService> m_service;
+  std::atomic<bool> m_running;
+  std::thread m_thread;
+};
 
-#endif
+} // namespace dunedaq::opmonlib
+
+#endif // OPMONLIB_INCLUDE_OPMONLIB_INFOMANAGER_HPP_
