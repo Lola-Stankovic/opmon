81cdcbc449eda196f538b9a44e7b135c3108ddd0
Joeal Subash <joeal.subash@cern.ch>
2022-07-22 16:52:05 +0100

    Issues.hpp macro replaced with class OpmonServiceCreationFailed

--- include/opmonlib/Issues.hpp
+++ include/opmonlib/Issues.hpp
@@ -12,10 +12,74 @@
 #include <ers/Issue.hpp>
 #include <string>
 
-namespace dunedaq {
+//namespace dunedaq {
 
-ERS_DECLARE_ISSUE(opmonlib, OpmonServiceCreationFailed, "OpmonServiceCreationFailed: " << error, ((std::string)error))
+//ERS_DECLARE_ISSUE(opmonlib, OpmonServiceCreationFailed, "OpmonServiceCreationFailed: " << error, ((std::string)error))
 
-} // namespace dunedaq
+//} // namespace dunedaq
+
+namespace dunedaq
+{                                                                                                                                                                                                              
+  namespace opmonlib{
+    class OpmonServiceCreationFailed : public ers::Issue{
+      template <class> friend class ers::IssueRegistrator;
+    protected:
+      OpmonServiceCreationFailed( const ers::Context & context ) : ers::Issue( context ) { ; }
+    public:
+      static const char * get_uid() {
+	return "opmonlib::OpmonServiceCreationFailed";
+      }
+      OpmonServiceCreationFailed(const ers::Context & context, std::string error);
+      OpmonServiceCreationFailed(const ers::Context & context, const std::string & msg , std::string error);
+      OpmonServiceCreationFailed(const ers::Context & context, const std::string & msg , std::string error, const std::exception & cause );
+      OpmonServiceCreationFailed( const ers::Context & context ,std::string error, const std::exception & cause );
+      void raise() const {
+	throw OpmonServiceCreationFailed(*this);
+      }
+      const char * get_class_name() const {
+	return get_uid();
+      }ers::Issue * clone() const {
+	return new opmonlib::OpmonServiceCreationFailed(*this);
+      } 
+      std::string get_error() {
+        std::string val;
+        ers::Issue::get_value("error", val );
+        return val;
+      }
+    };
+  }
+
+  namespace opmonlib
+  {
+    inline OpmonServiceCreationFailed::OpmonServiceCreationFailed
+    (const ers::Context & context , std::string error ) : ers::Issue( context ) {
+      set_value("error", error);
+      std::ostringstream out; out << "OpmonServiceCreationFailed: " << error;
+      prepend_message(out.str());
+    }
+
+    inline OpmonServiceCreationFailed::OpmonServiceCreationFailed
+    (const ers::Context & context, const std::string & msg , std::string error ) : ers::Issue( context, msg ) { set_value("error", error); }
+     
+    inline OpmonServiceCreationFailed::OpmonServiceCreationFailed
+    (const ers::Context & context, const std::string & msg , std::string error , const std::exception & cause ) : ers::Issue( context, msg , cause ) { set_value("error", error); }
+
+    inline OpmonServiceCreationFailed::OpmonServiceCreationFailed
+    (const ers::Context & context , std::string error, const std::exception & cause ) : ers::Issue( context , cause ) {set_value("error", error);
+
+      std::ostringstream out;
+      out << "OpmonServiceCreationFailed: " << error;
+      prepend_message( out.str() ); } }
+
+  namespace {
+
+    ers::IssueRegistrator<opmonlib::OpmonServiceCreationFailed>
+    opmonlib_OpmonServiceCreationFailed_instance;
+  }
+}
+
 
 #endif // OPMONLIB_INCLUDE_OPMONLIB_ISSUES_HPP_
+
+
+    
