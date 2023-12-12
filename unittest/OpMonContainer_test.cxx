926a066e5310700972a5d67db2b7c010d93e39b1
Marco Roda <mroda@liverpool.ac.uk>
2023-12-12 14:55:13 +0100

    Add file for unittest

---
+++ unittest/OpMonContainer_test.cxx
@@ -0,0 +1,18 @@
+/**
+ * @file OpMonContainer_test.cxx Test application that tests and demonstrates
+ * the functionality of the OpMonContainer class.
+ *
+ * This is part of the DUNE DAQ Application Framework, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+
+#include "opmonlib/OpMonContainer.hpp"
+
+#define BOOST_TEST_MODULE OpMonContainer_test // NOLINT
+
+using namespace dunedaq::opmonlib;
+
+BOOST_AUTO_TEST_SUITE(OpMonContainer_Test)
+
+
