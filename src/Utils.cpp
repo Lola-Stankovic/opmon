d0d21c656d16d1c371076e287f5daf8623ea862b
Marco Roda <mroda@liverpool.ac.uk>
2024-05-30 15:31:29 +0200

    Compiling and running nested structure, now to fix the unit tests

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -13,59 +13,10 @@ dunedaq::opmon::OpMonEntry dunedaq::opmonlib::to_entry(const google::protobuf::M
 
   dunedaq::opmon::OpMonEntry entry;
   entry.set_measurement( m.GetTypeName() );
-  
-  const auto * descriptor_p = m.GetDescriptor();
-  const auto & des = *descriptor_p;
-  
-  const auto * reflection_p = m.GetReflection();
-  const auto & ref = *reflection_p;
-  
-  using namespace google::protobuf;
 
-  auto count = des.field_count();
-  for ( int i = 0; i < count; ++i ) {
-    const auto * field_p = des.field(i);
-    if ( field_p -> is_repeated() ) continue;
-    
-    auto name = field_p -> name();
-    auto type = field_p -> cpp_type();
-    dunedaq::opmon::OpMonValue value;
-    bool success = true;
-    switch (type) {
-    case FieldDescriptor::CppType::CPPTYPE_INT32:
-      value.set_int4_value( ref.GetInt32(m, field_p) );
-      break;
-    case FieldDescriptor::CppType::CPPTYPE_INT64:
-      value.set_int8_value( ref.GetInt64(m, field_p) );
-      break;
-    case FieldDescriptor::CppType::CPPTYPE_UINT32:
-      value.set_uint4_value( ref.GetUInt32(m, field_p) );
-      break;
-    case FieldDescriptor::CppType::CPPTYPE_UINT64:
-      value.set_uint8_value( ref.GetUInt64(m, field_p) );
-      break;
-    case FieldDescriptor::CppType::CPPTYPE_DOUBLE:
-      value.set_double_value( ref.GetDouble(m, field_p) );
-      break;
-    case FieldDescriptor::CppType::CPPTYPE_FLOAT:
-      value.set_float_value( ref.GetFloat(m, field_p) );
-      break;
-    case FieldDescriptor::CppType::CPPTYPE_BOOL:
-      value.set_boolean_value( ref.GetBool(m, field_p) );
-      break;
-    case FieldDescriptor::CppType::CPPTYPE_STRING:
-      value.set_string_value( ref.GetString(m, field_p) );
-      break;
-    default:
-      success = false;
-      break;
-    } // switch on the cpp type                                                                         
-    
-      // it seems the following lines are causing the linking issue with the current system               
-    if ( success )
-      (*entry.mutable_data())[name] = value;
-  }
-
+  auto data = dunedaq::opmonlib::to_map(m);
+  entry.mutable_data()->insert(data.begin(), data.end());
+  
   for ( const auto & [tag, value] : co ) {
     (*entry.mutable_custom_origin())[tag] = value;
   }
@@ -74,8 +25,8 @@ dunedaq::opmon::OpMonEntry dunedaq::opmonlib::to_entry(const google::protobuf::M
 }
 
 
-dunedaq::opmonlib::map_type to_map( const google::protobuf::Message & m,
-				    std::string top_block) {
+dunedaq::opmonlib::map_type dunedaq::opmonlib::to_map( const google::protobuf::Message & m,
+						       std::string top_block) {
 
   dunedaq::opmonlib::map_type ret;
 
@@ -92,7 +43,8 @@ dunedaq::opmonlib::map_type to_map( const google::protobuf::Message & m,
     const auto * field_p = des.field(i);
     if ( field_p -> is_repeated() ) continue;
     
-    auto name = field_p -> name();
+    auto name = top_block.empty() ? field_p -> name() : top_block + '.' + field_p -> name(); 
+
     auto type = field_p -> cpp_type();
     dunedaq::opmon::OpMonValue value;
     bool success = true;
@@ -121,15 +73,21 @@ dunedaq::opmonlib::map_type to_map( const google::protobuf::Message & m,
     case FieldDescriptor::CppType::CPPTYPE_STRING:
       value.set_string_value( ref.GetString(m, field_p) );
       break;
+    case FieldDescriptor::CppType::CPPTYPE_MESSAGE:
+      {
+	auto data = dunedaq::opmonlib::to_map(ref.GetMessage(m, field_p), name);
+	ret.insert(data.begin(), data.end());
+	success = false;
+	break;
+      }
     default:
       success = false;
       break;
     } // switch on the cpp type                                                                         
 
-    auto complete_name = top_block.empty() ? name : top_block + '.' + name;
       
     if ( success )
-      ret[complete_name] = value;
+      ret[name] = value;
   }
 
   return ret;
